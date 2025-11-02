1b65a87b2b23b3bf0f4c1db88631f689ded4ede1
manp1 <bhagatnitin972@gmail.com>
2025-11-02 18:14:46 +0200

    Finished module02 task03

---
+++ Module01/Module02/Task03.py
@@ -0,0 +1,6 @@
+length=int(input("Enter the length of rectangle:"))
+width=int(input("Enter the width of rectangle:"))
+area=length*width
+perimeter=(length*2)+(width*2)
+print(f"The area of rectangle is:{area}")
+print(f"The perimeter of rectangle is:{perimeter}")
