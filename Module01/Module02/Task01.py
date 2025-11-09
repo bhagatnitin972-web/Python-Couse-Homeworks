60bdbf71d9dcea47829c6d60de6b9e6d6814b4a6
manp1 <bhagatnitin972@gmail.com>
2025-11-09 22:09:26 +0200

    Finished module03 task01

--- Module01/Module02/Task01.py
+++ Module01/Module02/Task01.py
@@ -1,2 +1,11 @@
-username = input("Please enter your name: ")
-print(f"Hello, {username}!")
+length = float(input("Enter the length of the zander in centimeters: "))
+
+
+legal_limit = 42
+
+
+if length < legal_limit:
+    shortage = legal_limit - length
+    print(f"The zander is {shortage:.1f} cm below the legal size limit. Please release it back into the lake.")
+else:
+    print("The zander meets the size requirement. You can keep it!")
--- Module01/Module02/Task05.py
+++
@@ -1,7 +0,0 @@
-talents=int(input("Enter talents:"))
-pounds=int(input("Enter pounds:"))
-lots=int(input("Enter lots:"))
-total_grams=((talents*20*32)+(pounds*32)+lots)*13.3
-kilograms=float(total_grams//1000)
-grams=total_grams%1000
-print("The weight in modern units:")
