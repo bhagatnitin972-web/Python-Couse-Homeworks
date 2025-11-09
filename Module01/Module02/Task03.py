508330608d2b8992b3857459f7281a74b136ec7c
manp1 <bhagatnitin972@gmail.com>
2025-11-09 22:29:00 +0200

    Finished module 03 task 03

--- Module01/Module02/Task03.py
+++ Module01/Module02/Task03.py
@@ -1,6 +1,21 @@
-length=int(input("Enter the length of rectangle:"))
-width=int(input("Enter the width of rectangle:"))
-area=length*width
-perimeter=(length*2)+(width*2)
-print(f"The area of rectangle is:{area}")
-print(f"The perimeter of rectangle is:{perimeter}")
+gender = input("Enter your biological gender (male/female): ").strip().lower()
+try:
+    hemoglobin = float(input("Enter your hemoglobin level (g/L): "))
+    if gender == "female":
+        if hemoglobin < 117:
+            print("Your hemoglobin level is LOW.")
+        elif hemoglobin <= 155:
+            print("Your hemoglobin level is NORMAL.")
+        else:
+            print("Your hemoglobin level is HIGH.")
+elif gender == "male":
+        if hemoglobin < 134:
+            print("Your hemoglobin level is LOW.")
+        elif hemoglobin <= 167:
+            print("Your hemoglobin level is NORMAL.")
+        else:
+            print("Your hemoglobin level is HIGH.")
+else:
+        print("Invalid gender entered. Please enter 'male' or 'female'.")
+except ValueError:
+    print("Invalid input. Please enter a numeric value for hemoglobin.")
